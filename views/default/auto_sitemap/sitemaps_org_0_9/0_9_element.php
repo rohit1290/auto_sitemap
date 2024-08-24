2fc91376036fd4ce897385df635b5edd036efbfb
Rohit Gupta <rohit_1290@yahoo.com>
2024-08-24 14:33:28 +0530

    fix: minor code fix

--- views/default/auto_sitemap/sitemaps_org_0_9/0_9_element.php
+++ views/default/auto_sitemap/sitemaps_org_0_9/0_9_element.php
@@ -5,15 +5,25 @@ $body ='';
 foreach ($vars['urls'] as $element) {
 		$body .= '<url>';
 		$body .= '<loc>' . trim($element['loc']) . '</loc>';
-	if ( $element['priority'] != 'none') {
-		$body .= '<priority>' . $element['priority'] . '</priority>';
-	}
-	if ( $element['lastmod'] ) {
-		$body .= '<lastmod>' . date('Y-m-d', $element['lastmod']  ) . '</lastmod>';
-	}
-	if ( $element['changefreq'] && $element['changefreq'] != 'disabled') {
-		$body .= '<changefreq>' . $element['changefreq'] . '</changefreq>';
-	}
+		
+		if(array_key_exists('priority', $element)){
+			if ( $element['priority'] != 'none') {
+				$body .= '<priority>' . $element['priority'] . '</priority>';
+			}
+		}
+
+		if(array_key_exists('lastmod', $element)){
+			if ( $element['lastmod'] ) {
+				$body .= '<lastmod>' . date('Y-m-d', $element['lastmod']  ) . '</lastmod>';
+			}
+		}
+
+		if(array_key_exists('changefreq', $element)){
+			if ( $element['changefreq'] && $element['changefreq'] != 'disabled') {
+				$body .= '<changefreq>' . $element['changefreq'] . '</changefreq>';
+			}
+		}
+
 		$body .= '</url>';
 }
 
