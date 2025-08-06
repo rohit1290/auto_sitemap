b93e68994eac24e0c8e56a7068e1a41812ebfbca
Rohit Gupta <rohit_1290@yahoo.com>
2025-08-06 18:54:38 +0530

    update: element sequency based on sitemap rule

--- views/default/auto_sitemap/sitemaps_org_0_9/0_9_element.php
+++ views/default/auto_sitemap/sitemaps_org_0_9/0_9_element.php
@@ -5,12 +5,6 @@ foreach ($vars['urls'] as $element) {
 	$body .= '<url>';
 	$body .= '<loc>' . trim($element['loc']) . '</loc>';
 	
-	if(array_key_exists('priority', $element)){
-		if ( $element['priority'] != 'none') {
-			$body .= '<priority>' . $element['priority'] . '</priority>';
-		}
-	}
-	
 	if(array_key_exists('lastmod', $element)){
 		if ( $element['lastmod'] ) {
 			$body .= '<lastmod>' . date('Y-m-d', $element['lastmod']  ) . '</lastmod>';
@@ -20,6 +14,12 @@ foreach ($vars['urls'] as $element) {
 	if(array_key_exists('changefreq', $element)){
 		if ( $element['changefreq'] && $element['changefreq'] != 'disabled') {
 			$body .= '<changefreq>' . $element['changefreq'] . '</changefreq>';
+		}
+	}
+	
+	if(array_key_exists('priority', $element)){
+		if ( $element['priority'] != 'none') {
+			$body .= '<priority>' . $element['priority'] . '</priority>';
 		}
 	}
 	
